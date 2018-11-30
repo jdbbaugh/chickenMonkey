f1e7b31ba854feeb7c211e18d54f6a402335ed43
jdbbaugh <jbaugh78@msn.comgit>
2018-11-30 11:08:28 -0600

    i missed the part in the directions about chickenmonkey.... fixed it

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
     // Divide the current number by 2, and check if the remainder is 0
-    if (currentNumber % 5 === 0) {
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) { 
+        console.log('chickenmonkey');
+    } else if (currentNumber % 5 === 0) {
         console.log('chicken');
+        
     } else if (currentNumber % 7 === 0) {
-        console.log('monkey');
+        console.log('monkey');;
     } else {
         console.log(currentNumber);
     }
